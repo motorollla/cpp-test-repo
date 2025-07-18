cdb5ae8857ed911a1839bd16c52c269cc836e0df
artem <ka954artem@gmail.com>
2025-07-18 22:57:30 +0300

    обавлен файл bad.cpp

---
+++ bad.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Artem on 18.07.2025.
+//
+
+#include "bad.h"
---
+++ bad.h
@@ -0,0 +1,14 @@
+//
+// Created by Artem on 18.07.2025.
+//
+
+#ifndef CPP_TEST_REPO_BAD_H
+#define CPP_TEST_REPO_BAD_H
+
+
+class bad {
+
+};
+
+
+#endif //CPP_TEST_REPO_BAD_H
